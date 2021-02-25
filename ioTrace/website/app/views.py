2658127ae68eaff3d95cbc09be587fb06649c365
jwalkern <56714873+jwalkern@users.noreply.github.com>
2021-02-25 12:51:53 +0100

    123

--- ioTrace/website/app/views.py
+++ ioTrace/website/app/views.py
@@ -10,3 +10,22 @@ def index():
     
     return render_template('index.html')
 
+@app.route('/create')
+def create():
+    
+    return render_template('create.html')
+
+@app.route('/read')
+def read():
+    
+    return render_template('read.html')
+
+@app.route('/update')
+def update():
+    
+    return render_template('update.html')
+
+@app.route('/delete')
+def delete():
+    
+    return render_template('delete.html')
